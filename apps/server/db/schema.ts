107f776a4b83c05b0423d4f7d4109979e38cd370
korade-krushna <krishnacodes15@gmail.com>
2026-01-08 23:44:48 +0530

    Add Drizzel & Schema

---
+++ apps/server/db/schema.ts
@@ -0,0 +1,14 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core"
+
+
+export const employeesTable = pgTable("employees", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar().notNull(),
+  position: varchar().notNull(),
+});
+
+export const modelsTable = pgTable("models", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar().notNull(),
+  version: varchar().notNull(),
+});
