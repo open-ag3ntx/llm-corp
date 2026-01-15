25c9ccf00b804338f08538d76800bcdec1d55b16
korade-krushna <krishnacodes15@gmail.com>
2026-01-15 23:44:32 +0530

    Format Code

--- apps/server/db/schema.ts
+++ apps/server/db/schema.ts
@@ -1,20 +1,2 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core"
 
-
-export const employeesTable = pgTable("employees", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar().notNull(),
-  position: varchar().notNull(),
-});
-
-export const modelsTable = pgTable("models", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar().notNull(),
-  version: varchar().notNull(),
-});
-
-export const tasksTable = pgTable("tasks", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  description: varchar().notNull(),
-  status: varchar().notNull(),
-});
