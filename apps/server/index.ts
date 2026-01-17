af07ad2d346f31a30d04341c5630a0acfec4f84e
korade-krushna <krishnacodes15@gmail.com>
2026-01-17 23:30:18 +0530

    Init Chat Route

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -9,15 +9,15 @@ import swagger from "@elysiajs/swagger";
 const port = 8080;
 const app = new Elysia()
   .use(
-    swagger({
-      documentation: {
-        info: {
-          title: "llm-corp engine",
-          description: "llm-corp engine API",
-          version: "1.0.0",
-        },
-      },
-    }),
+    // swagger({
+    //   documentation: {
+    //     info: {
+    //       title: "llm-corp engine",
+    //       description: "llm-corp engine API",
+    //       version: "1.0.0",
+    //     },
+    //   },
+    // }),
   )
   .onBeforeHandle(() => {
     console.log(`Server is running on http://localhost:${port}`);
